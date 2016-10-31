5f56e793fcb13ec3364ff99633b3d93fc230e01a
Yujing Lou <865227010@qq.com>
2016-10-31 19:42:43 +0800

    Add actions about user's login

---
+++ src/com/a1rdr0p/SEproject/action/login.java
@@ -0,0 +1,17 @@
+package com.a1rdr0p.SEproject.action;
+
+import com.opensymphony.xwork2.Action;
+
+/**
+ * @author Loj
+ *
+ */
+public class login implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
