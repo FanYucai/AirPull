bd6bbbf450534adcf35a4d22ff67d1f1084147a9
Yujing Lou <865227010@qq.com>
2016-11-10 22:23:58 +0800

    Sign up 1.5

--- src/com/a1rdr0p/SEproject/action/signUp.java
+++ src/com/a1rdr0p/SEproject/action/signUp.java
@@ -10,6 +10,17 @@ import com.opensymphony.xwork2.Action;
  */
 public class signUp implements Action {
 	private User user;
+	private String verifyPassword;
+	
+	public String getVerifyPassword() {
+		return verifyPassword;
+	}
+
+
+	public void setVerifyPassword(String verifyPassword) {
+		this.verifyPassword = verifyPassword;
+	}
+
 
 	public User getUser() {
 		return user;
@@ -25,11 +36,16 @@ public class signUp implements Action {
 		userService us = new userService();
 		String ret = "";
 		
+		boolean flag = user.getPassword().equals(verifyPassword);
+		
 		int verify = us.newUser(user.getName(), user.getPassword());
 //		System.out.println(user.getName());
 //		System.out.println(user.getPassword());
 //		System.out.println(verify);
-		if (verify == 1) {
+		if (flag == false) {
+			ret = "notMatch";
+		}
+		else if (verify == 1) {
 			ret = SUCCESS;
 		}
 		else if (verify == 110) {
