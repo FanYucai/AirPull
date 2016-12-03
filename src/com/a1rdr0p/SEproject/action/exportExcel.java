6f00adabd2c309e9636e1ef1ca41195ce21b94c0
FanYucai <fanyucai@126.com>
2016-12-03 21:40:15 +0800

    更改了导出xls文件的方式，采用POI，并且能够导出合并表格

--- src/com/a1rdr0p/SEproject/action/exportExcel.java
+++ src/com/a1rdr0p/SEproject/action/exportExcel.java
@@ -3,7 +3,10 @@ package com.a1rdr0p.SEproject.action;
 import java.awt.*;
 import java.util.*;
 import java.io.*;
-import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.Action;  
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.hssf.util.CellRangeAddress;
+import org.apache.poi.hssf.util.Region; 
 
 public class exportExcel implements Action {
 
@@ -26,19 +29,34 @@ public class exportExcel implements Action {
     } 
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
-        StringBuffer sb = new StringBuffer();               
-        //\t表示下一列，\n表示下一行   
-//        sb.append("title1").append("\t").append("title2").append("\n");  
-//        //导入的数据进行迭代，拼成字符串  
-//        for(int i=0; i<3; i++){   
-//            sb.append("篓子").append("\t").append("qwq").append("\n");   
-//        }   
-//        //将字符串放入流中
-        
-        sb.append(fileContent);
-        excelStream = new ByteArrayInputStream(sb.toString().getBytes("GBK"), 
-        		0, sb.toString().getBytes("GBK").length); 
+//        StringBuffer sb = new StringBuffer();               
+//        //\t表示下一列，\n表示下一行          
+//        sb.append(fileContent);
+//        excelStream = new ByteArrayInputStream(sb.toString().getBytes("GBK"), 
+//        		0, sb.toString().getBytes("GBK").length); 
 
+		HSSFWorkbook wb = new HSSFWorkbook();
+		HSSFCellStyle style = wb.createCellStyle();
+		style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+		style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+//		style.setAlignment(HSSFCellStyle.VERTICAL_JUSTIFY);
+		
+		HSSFSheet sheet = wb.createSheet();
+		sheet.addMergedRegion(new CellRangeAddress(1,1,6,7));
+		for(int i=0; i<10; i++) {
+			HSSFRow r = sheet.createRow(i);
+			for(int k=0; k<10; k++) {
+				HSSFCell c = r.createCell(k);
+				String value = "123";
+				c.setCellValue(value);
+				c.setCellStyle(style);
+			}
+		}
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		wb.write(os);
+		byte[] content = os.toByteArray();
+		excelStream = new ByteArrayInputStream(content);
+		
 	    return "excel";
 	}
 
