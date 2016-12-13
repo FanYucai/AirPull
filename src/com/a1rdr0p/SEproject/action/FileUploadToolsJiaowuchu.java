8579dfaf013c73ccfdfe8b6843ebb0de55a68302
FanYucai <fanyucai@126.com>
2016-12-14 01:04:40 +0800

    教务处转码

--- src/com/a1rdr0p/SEproject/action/FileUploadToolsJiaowuchu.java
+++ src/com/a1rdr0p/SEproject/action/FileUploadToolsJiaowuchu.java
@@ -9,6 +9,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.apache.struts2.ServletActionContext;
 
@@ -115,8 +116,11 @@ public class FileUploadToolsJiaowuchu {
 				for (int j = 0; j < tdItem.size(); j++) {
 					System.out.println(tdItem.get(j).html());
 					if (j!=tdItem.size()-1) {
-						tmp_ += tdItem.get(j).text();
+						byte[] encodeBase64 = Base64.encodeBase64(tdItem.get(j).text().getBytes("UTF-8")); 
+						tmp_ += new String(encodeBase64);
+						
 						if(tdItem.get(j).hasAttr("colspan")||tdItem.get(j).hasAttr("rowspan")) {
+							
 							String rowspan="", colspan="";
 							rowspan = tdItem.get(j).attr("rowspan");
 							colspan = tdItem.get(j).attr("colspan");
@@ -132,7 +136,8 @@ public class FileUploadToolsJiaowuchu {
 						}
 						tmp_ += "@";						
 					} else {
-						tmp_ += tdItem.get(j).text();
+						byte[] encodeBase64 = Base64.encodeBase64(tdItem.get(j).text().getBytes("UTF-8")); 
+						tmp_ += new String(encodeBase64);
 						if(tdItem.get(j).hasAttr("colspan")||tdItem.get(j).hasAttr("rowspan")) {
 							String rowspan="", colspan="";
 							rowspan = tdItem.get(j).attr("rowspan");
